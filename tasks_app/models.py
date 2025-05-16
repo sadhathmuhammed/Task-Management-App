4b3315de7b1bd549cc741fea3447c53c610baf32
Muhammed Sadhath <muhammed.sadhath@scrapehero.com>
2025-05-16 20:08:48 +0530

    models completed and migrated

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=500)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    due_date = models.DateField()
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('INPROGRESS', 'InProgress'),
+        ('COMPLETED', 'Completed'),
+    )
+    status = models.CharField(default="PENDING", choices=STATUS_CHOICES, max_length=20)
+    completion_report = models.TextField()
+    worked_hours = models.FloatField()
+
+
